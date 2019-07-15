dec5cbb0b968495bf5f7a266d5f1cc950c8dde48
yjt-1997 <jiangtap1997qqq@qq.com>
2019-07-15 22:03:24 +0800

    create project

---
+++ src/test/java/GradleTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class GradleTest {
+
+    @Test
+    public void testMockClass() throws Exception {
+        // you can mock concrete classes, not only interfaces
+        LinkedList mockedList = mock(LinkedList.class);
+
+        // stubbing appears before the actual execution
+        String value = "first";
+        when(mockedList.get(0)).thenReturn(value);
+
+        assertEquals(mockedList.get(0), value);
+    }
+}
